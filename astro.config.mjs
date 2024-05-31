bba06aa42bc1e1b6d40be78a82de8ff6b4256c21
Jonathan Simon <yuditbertoli@gmail.com>
2024-05-31 19:12:04 -0400

    chore: Update Astro configuration with site, integrations, and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import { SITE } from "./src/config";
+import preact from "@astrojs/preact";
+import mdx from '@astrojs/mdx';
+import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE.website,
+  integrations: [preact(), tailwind(), mdx(), sitemap()]
+});
