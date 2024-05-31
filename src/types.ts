0cbccde59fea82915f340fd95b94bea4e927b4bc
Jonathan Simon <yuditbertoli@gmail.com>
2024-05-31 19:12:33 -0400

    chore: Add Site type definition

---
+++ src/types.ts
@@ -0,0 +1,10 @@
+
+export type Site = {
+  website: string;
+  author: string;
+  desc: string;
+  title: string;
+  ogImage?: string;
+  lightAndDarkMode: boolean;
+  scheduledPostMargin: number;
+};
