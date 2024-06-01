9c56e42716512f2cf7008f221df50010e43d2b94
Jonathan Simon <yuditbertoli@gmail.com>
2024-06-01 04:00:47 -0400

    feat: Remove unused LOCALE constant
    The commit removes the unused LOCALE constant from the config.ts file. This constant was previously defined but not used in the codebase. Removing it improves code clarity and reduces unnecessary clutter.

--- src/config.ts
+++ src/config.ts
@@ -10,11 +10,6 @@ export const SITE: Site = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
-export const LOCALE = {
-  lang: "es", // html lang code. Set this empty and default will be "en"
-  langTag: ["es-ES"], // BCP 47 Language Tags. Set this empty [] to use the environment default
-} as const;
-
 export const LOGO_IMAGE = {
   enable: false,
   svg: true,
