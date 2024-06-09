08da1c380a00e3565c09dc2b8e05913df332563e
Jonathan Simon <yuditbertoli@gmail.com>
2024-06-09 18:11:24 -0400

    feat: Update site description in i18n/ui.ts
    The site description in the i18n/ui.ts file has been updated in both English and Spanish translations. This change provides a more accurate and engaging description for the personal blog, improving the overall user experience.

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,12 +11,14 @@ export const ui = {
     'nav.blog': 'Blog',
     'nav.etiquetas': 'Etiquetas',
     'nav.sobre-mi': 'Sobre mí',
+    'site.desc': "Espero y disfrutes de mi blog personal, donde comparto contenido sobre tecnología, Flutter, curiosidades y básicamente cualquier cosa que se me ocurra, espero que disfrutes de este blog y que aprendas algo nuevo.",
   },
   en: {
     'nav.inicio': 'Home',
     'nav.blog': 'Blog',
     'nav.etiquetas': 'Tags',
     'nav.sobre-mi': 'About me',
+    'site.desc': "I hope you enjoy my personal blog, where I share content about technology, Flutter, curiosities and basically anything that occurs to me, I hope you enjoy this blog and learn something new.",
   },
 } as const;
 
