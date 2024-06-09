355e3b0d969e1196efdd200b8b68cae62a3d515d
Jonathan Simon <yuditbertoli@gmail.com>
2024-06-09 17:31:43 -0400

    refactor: Update astro.config.mjs to use @astrojs/vercel/static adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,23 +6,26 @@ import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import vercel from '@astrojs/vercel/static';
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE.website,
   integrations: [preact(), tailwind(), mdx(), sitemap()],
   vite: {
     optimizeDeps: {
-      exclude: ["@resvg/resvg-js"],
-    },
+      exclude: ["@resvg/resvg-js"]
+    }
   },
   scopedStyleStrategy: "where",
   i18n: {
     defaultLocale: "es",
     locales: ["en", "es", "zh"],
     routing: {
-      prefixDefaultLocale: false,
+      prefixDefaultLocale: false
     }
   },
-  adapter:  vercel({
-    webAnalytics: { enabled: true }
-  }),
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true
+    }
+  })
 });
