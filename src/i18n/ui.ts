7a1f06f3814f56fac8584933cbca97819cc0b604
JSimonDev <jsimondev@gmail.com>
2024-06-26 18:07:14 -0400

    feat: Add portfolio link to navigation bar

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -11,6 +11,7 @@ export const ui = {
     'nav.blog': 'Blog',
     'nav.etiquetas': 'Etiquetas',
     'nav.sobre-mi': 'Sobre mí',
+    'nav.portafolio': 'Portafolio',
     'site.desc': "Espero y disfrutes de mi blog personal, donde comparto contenido sobre tecnología, Flutter, curiosidades y básicamente cualquier cosa que se me ocurra, espero que disfrutes de este blog y que aprendas algo nuevo.",
   },
   en: {
@@ -18,6 +19,7 @@ export const ui = {
     'nav.blog': 'Blog',
     'nav.etiquetas': 'Tags',
     'nav.sobre-mi': 'About me',
+    'nav.portafolio': 'Portfolio',
     'site.desc': "I hope you enjoy my personal blog, where I share content about technology, Flutter, curiosities and basically anything that occurs to me, I hope you enjoy this blog and learn something new.",
   },
 } as const;
